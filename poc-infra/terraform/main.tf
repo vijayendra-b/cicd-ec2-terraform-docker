c508cb5ab77fdfad010d14d36e7f7e5f3c1424d3
vijayendra-b <vijayendraswamy@gmail.com>
2026-02-06 09:57:18 +0530

    updated files for terraform

---
+++ poc-infra/terraform/main.tf
@@ -0,0 +1,45 @@
+resource "aws_security_group" "node_sg" {
+  name = "node-app-sg"
+
+  ingress {
+    from_port   = 3000
+    to_port     = 3000
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+resource "aws_instance" "node_ec2" {
+  ami           = "ami-0c02fb55956c7d316" # Amazon Linux
+  instance_type = "t2.micro"
+  key_name      = var.key_name
+
+  vpc_security_group_ids = [aws_security_group.node_sg.id]
+
+  user_data = <<-EOF
+              #!/bin/bash
+              yum update -y
+              amazon-linux-extras install docker -y
+              systemctl start docker
+              systemctl enable docker
+              usermod -aG docker ec2-user
+              EOF
+
+  tags = {
+    Name = "node-app-ec2"
+  }
+}
